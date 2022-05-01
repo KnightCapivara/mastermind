bde3765ff72d88e6750ccaecc4641ad272842432
Danillo <danillo.targino@gmail.com>
2022-05-01 13:52:14 -0400

    update logic game.rb

--- game.rb
+++ game.rb
@@ -15,11 +15,10 @@ class Game
 
     def turn
         puts "Digite quatro números (1 a 6) para adivinhar o código"
-
-    loop do
-        @guess = gets.chomp
-        break if @guess.match(/[1-6][1-6][1-6][1-6]/) && @guess.length == 4
-        puts "Seu palpite deve ter apenas 4 dígitos entre 1-6.".red
+        loop do
+            @guess = gets.chomp
+            break if @guess.match(/[1-6][1-6][1-6][1-6]/) && @guess.length == 4
+            puts "Seu palpite deve ter apenas 4 dígitos entre 1-6.".red
         end
         self.reveal(@guess.split(//))
         puts "Descobra a lógica de comparação:"
@@ -27,20 +26,36 @@ class Game
     end
     
     def compare (guess)
-        guess.each do |num|
-        print " ? ".reverse_color if @master_code.numbers.include?(num)
-        print " " if @master_code.numbers.include?(num)
+        temp_master = []
+        @master_code.numbers.each { |num| temp_master << num }
+        index = 0
+        4.times do
+          if temp_master[index] == guess[index]
+            print " * ".bg_black.gray
+            print " "
+            temp_master[index] = "X"
+            guess[index]  = "X"
+          end
+          index += 1
         end
-        puts ""
-        # puts "Game Clues:"
-        # print " * ".bg_black.gray
-        # print " Correct Color & Correct Spot"
-        # puts ""
-        # print " ? ".reverse_color
-        # print " Correct Color"
-        # puts ""
+        i = 0
+        4.times do
+          if guess[i] != "X" && temp_master.include?(guess[i])
+            print " ? ".reverse_color
+            print " "
+            delete = temp_master.find_index(guess[i])
+            temp_master[delete] = "X"
+            guess[i]  = "X"
+          end
+        i += 1
     end
     
+    puts ""
+        # puts "#{guess} é o novo palpite"
+        # puts "#{temp_master} é o novo mestre temporário"
+        # puts "#{@master_code.numbers} deve ser inalterado"
+    end
+
     def reveal (array)
         array.each do | num |
             print "  #{num}  ".bg_red.bold if num == 1 || num == "1"
@@ -60,9 +75,45 @@ class Game
         self.show
         self.turn
     end
+
 end
 
 # método: instruções
 # método: obtém suposições do código do usuário
 # método: adivinhar o código do processo
 # método: mostra código e pistas
+
+# CODE:  3234
+# guess: 2335
+# Verifique os valores de índice que correspondem
+# CODE:  32X4
+# guess: 23X5
+# Verifique para o mesmo número
+# 2
+# CODE:  3XX4
+# guess: X3X5
+# 3
+# CODE:  XXX4
+# guess: XXX5
+# 5
+# CODE:  XXX4
+# guess: XXXX
+
+# CODE:  1422
+# guess: 2344
+# Verifique os valores de índice que correspondem
+# CODE:  1422
+# guess: 2344
+# Verifique para o mesmo número
+# 2
+# CODE:  14X2
+# guess: X344
+# 3
+# CODE:  14X2
+# guess: XX44
+# 4
+# CODE:  1XX2
+# guess: XXX4
+# 4
+# CODE:  1XX2
+# guess: XXXX
