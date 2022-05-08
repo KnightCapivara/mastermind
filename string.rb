dd981cb8ab4aa1fb7857afd0559ddeaf36622cff
Danillo <danillo.targino@gmail.com>
2022-05-07 22:08:26 -0400

    edit string.rb

--- string.rb
+++ string.rb
@@ -3,7 +3,7 @@ class String
     def red;            "\e[31m#{self}\e[0m" end
     def black;          "\e[30m#{self}\e[0m" end
     def green;          "\e[32m#{self}\e[0m" end
-  
+    
     def bg_black;       "\e[40m#{self}\e[0m" end
     def bg_red;         "\e[41m#{self}\e[0m" end
     def bg_green;       "\e[42m#{self}\e[0m" end
@@ -12,7 +12,8 @@ class String
     def bg_magenta;     "\e[45m#{self}\e[0m" end
     def bg_cyan;        "\e[46m#{self}\e[0m" end
     def bg_gray;        "\e[47m#{self}\e[0m" end
-    
+  
     def bold;           "\e[1m#{self}\e[22m" end
-    def reverse_color;  "\e[7m#{self}\e[27m" end
+    # def reverse_color;  "\e[7m#{self}\e[27m" end
+    def underline;      "\e[4m#{self}\e[24m" end
 end
