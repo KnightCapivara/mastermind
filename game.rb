6dd3edff75c4ad3a8603abccb6735aa9f9115194
Danillo <danillo.targino@gmail.com>
2022-05-07 21:51:28 -0400

    update game

--- game.rb
+++ game.rb
@@ -1,12 +1,33 @@
 class Game
+
     def start
-      puts "Vamos começar a jogar Mastermind"
+      puts ""
+      puts "Vamos jogar MASTERMIND"
       @master_code = Code.new
       @options = ["1", "2", "3", "4", "5", "6"]
-      puts "O computador irá escolher 4 cores/números aleatórios para fazer um código"
+      puts "Este jogo é jogado com as seguintes combinações de números/cores:"
       self.reveal(@options)
       puts ""
+      puts ""
+      puts "O computador escolherá aleatoriamente 4 numeros para criar um código, para você decifrar. Por exemplo,"
+      self.reveal(["5", "6", "2", "4"])
+      puts ""
+      puts ""
+      puts "Pode haver mais de um com o mesmo número/cor. Por exemplo,".red
+      self.reveal(["2", "1", "3", "2"])
+      puts ""
+      puts ""
+      puts "O computador pode lhe dar uma pista para ajudá-lo a resolver para cada, UM DE CADA!!!"
+      print " * ".bg_gray.green
+      print " "
+      puts "Esta pista significa que você tem 1 número correto no local correto."
+      puts ""
+      print " ? ".bg_gray.red
+      print " "
+      puts "Essa pista significa que você tem 1 número correto, mas está no local errado."
+      puts ""
     end
+  
     def show
       puts "CÓDIGO MASTER REVELADO TEMPORARIAMENTE:"
       self.reveal(@master_code.numbers)
@@ -14,16 +35,27 @@ class Game
     end 
   
     def turn
-      loop do
-        puts "Digite quatro números (1 a 6) para adivinhar o código"
+      turn = 1
+      12.times do
+        puts "Turno ##{turn}: Digite quatro números (1-6) para adivinhar o código"
+        puts "Pense com cuidado. Este é o seu último turno para ganhar!".red if turn == 12
+        turn += 1
         loop do
           @guess = gets.chomp
           break if @guess.match(/[1-6][1-6][1-6][1-6]/) && @guess.length == 4
-          puts "Seu palpite deve ter apenas 4 dígitos entre 1-6.".red
+          puts "Seu palpite deve ser de apenas 4 dígitos entre 1-6.".red
         end
         self.reveal(@guess.split(//))
         break if solved?(@master_code.numbers, @guess.split(//))
         self.compare(@guess.split(//))
+      end
+      if solved?(@master_code.numbers, @guess.split(//))
+        puts "  Você desbloqueou o código! Parabéns, você venceu!"
+      else
+        puts "Game over. Você ficou sem turnos. ¯\\_(ツ)_/¯ ".red
+        puts "Aqui está o código que você estava tentando decifrar:"
+        self.reveal(@master_code.numbers)
+        puts ""
       end
     end
   
@@ -57,16 +89,15 @@ class Game
         # puts "#{temp_master} é o novo mestre temporário"
         # puts "#{@master_code.numbers} deve ser inalterado"
     end
-  
+
     def solved? (master, guess)
       game_over = false
       if master[0] == guess[0] && master[1] == guess[1] && master[2] == guess[2] && master[3] == guess[3]
-        puts "  Solução correta! Game Over!!!"
         game_over = true
       end
       game_over
     end
-  
+
     def reveal (array)
       array.each do | num |
         print "  #{num}  ".bg_blue if num == "1"
@@ -78,13 +109,14 @@ class Game
         print " "
       end
     end
-  
+
     def play
       self.start
       self.show
       self.turn
     end
-  
-  end
-  
-  # metodo: instruções
+
+end
+
+
+  # method: instruções
